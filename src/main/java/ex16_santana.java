995da013b9131a555130b20aea5a82cd5719d025
dereks7 <90410123+dereks7@users.noreply.github.com>
2021-09-12 18:09:42 -0400

    Initial commit

---
+++ src/main/java/ex16_santana.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+/*
+What is your age? 15
+You are not old enough to legally drive.
+
+What is your age? 35
+You are old enough to legally drive.
+ */
+
+public class ex16_santana
+{
+    public static void main( String[] args ) {
+        System.out.println("What is your age?");
+        Scanner in1 = new Scanner(System.in);
+        String age = in1.nextLine();
+        int n1 = Integer.parseInt(age);
+
+        if (n1 > 15)
+        {
+            System.out.println("You are old enough to legally drive.");
+        }
+        else
+        {
+            System.out.println("You are not old enough to legally drive.");
+        }
+    }
+}
